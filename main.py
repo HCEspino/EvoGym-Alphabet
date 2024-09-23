a5242b7ed14df0d72a997cfeb849c4ee1ad55bb4
Harrison Espino <58458258+HCEspino@users.noreply.github.com>
2024-09-22 19:22:57 -0700

    fixed parameters

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from genetic_alg import GeneticAlgorithm
 
-ga = GeneticAlgorithm(population_size=6, generations=2, shape_file=None)
+ga = GeneticAlgorithm(population_size=16, generations=50, shape_file=None)
 while ga.generation_count <= ga.generations:
     print(f"Generation {ga.generation_count}")
-    ga.train_population(training_steps=1)
-    ga.evaluate_population(episodes=1, steps=100, verbose=1)
+    ga.train_population(training_steps=50000)
+    ga.evaluate_population(episodes=1, steps=500, verbose=1)
     ga.evolve()
