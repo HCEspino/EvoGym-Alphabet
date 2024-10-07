b326f38c40bd21ca366253f4cc1311b7b2a63141
Harrison Espino <58458258+HCEspino@users.noreply.github.com>
2024-10-06 22:12:16 -0700

    A-M

--- shape_saver.py
+++ shape_saver.py
@@ -3,17 +3,17 @@ import numpy as np
 # Create the default 8x8 letter 'A' shape
 robot_shape = np.array([
     [0, 1, 1, 1, 1, 1, 1, 0],
-    [0, 1, 1, 1, 1, 1, 1, 0],
-    [0, 1, 1, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 1, 1, 0, 0],
-    [0, 1, 1, 1, 1, 1, 0, 0],
-    [0, 1, 1, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 0, 0, 0, 0, 1, 1],
+    [1, 1, 0, 0, 0, 0, 1, 1],
+    [1, 1, 0, 0, 0, 0, 1, 1],
+    [1, 1, 0, 0, 0, 0, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
     [0, 1, 1, 1, 1, 1, 1, 0],
 ])
 
 # Save the array to a file
-file_path = 'E.npy'
+file_path = 'Ov2.npy'
 np.save(file_path, robot_shape)
 
 print(f"Robot shape saved to {file_path}")
